09c4628e8958ae2d902e11595831f8c007f211ab
Dia6lo <Dia6lo@mail.ru>
2015-04-17 18:04:42 +1100

    Add reading file Add tests

--- BrainSharp/Interpreter.cs
+++ BrainSharp/Interpreter.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BrainSharp
 {
-    static class Interpreter
+    public static class Interpreter
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: {0} <path to a source file>", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+            var filePath = args[0];
+            if (!File.Exists(filePath))
+                Console.WriteLine("Not a source file " + filePath);
+            var program = new Program(File.ReadAllText(filePath));
         }
     }
 }
---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        private string testFolder = "Test Cases/";
+
+        [TestMethod]
+        public void ExecuteProgram()
+        {
+            BrainSharp.Interpreter.Main(new [] {testFolder + "Hello World.txt"});
+        }
+    }
+}
