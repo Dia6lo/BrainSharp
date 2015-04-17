8f899555b33b5662a127660a48733c1751885c37
Dia6lo <Dia6lo@mail.ru>
2015-04-17 17:26:29 +1100

    Init

---
+++ BrainSharp/Interpreter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainSharp
+{
+    static class Interpreter
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
